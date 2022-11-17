4a1bf1358857d7c4c8e19f7d8069d6e0d6436300
José Miguel Barriuso <josemiguelbarriuso@Joses-MacBook-Pro.local>
2022-11-17 19:44:49 -0300

    creacion fuincion multiplicacion

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const a = 3;
 const b = 4;
 
 
-const suma = (a,b) => {
-    
+const suma = (a,b) => {   
     return a+b;
+}
 
+const multiply = (a,b) => {
+
+    return a*b
 }
 
 console.log(suma(a,b));
+console.log(multiply(a,b));
