ca3b2722e96a40c88c02f11955fa57aedfbc08ac
Josemaria Naranjo <josemnaranjoc@gmail.com>
2022-11-17 19:56:36 -0300

    Afeando codigo

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ const b = 4;
 
 
 
+console.log(a);
 const suma = (a,b) => {   
     return a+b+1;
 }
 
 const multiply = (a,b) => {
 
-    return a*b
+
+
+
+    return a*b*2
 }
 
 console.log(suma(a,b));
